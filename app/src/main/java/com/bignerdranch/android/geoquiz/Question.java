cf4a77062e3941279ba6fed591c30beb7a447cb2
Mack Costin <mack_costin@hotmail.com>
2017-08-06 17:58:17 +1000

    Finished Version of the Week 2 Tutorial Task

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by Mack on 04-Aug-17.
+ */
+
+public class Question {
+    private int mTextResID;
+    private boolean mAnswerTrue;
+
+    public Question(int textResID, boolean answerTrue){
+        mTextResID = textResID;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResID() {
+        return mTextResID;
+    }
+
+    public void setTextResID(int textResID) {
+        mTextResID = textResID;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
